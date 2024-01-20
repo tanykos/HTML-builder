74c4fda51f4b35bc0f3b8843e9d52640c4c1cbc1
Tanya Kostromitskaya <tetian.kos@yandex.ru>
2024-01-20 21:24:18 +0100

    feat: implement  01-read-file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+stream.on('data', (chunk) => {
+  console.log(chunk);
+});
